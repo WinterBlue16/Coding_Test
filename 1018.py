ee399d260d3aafcef53de340f7a039f3dc9694fd
Winter Lee <leekh090163@gmail.com>
2020-03-05 11:33:09 +0900

    18번 문제

---
+++ 1018.py
@@ -0,0 +1,13 @@
+# 풀이 1
+a, b = input().split(':')
+print(a, b, sep=":")
+
+# 풀이 2
+a, b = input().split(':')
+x = int(a)
+y = int(b)
+print(x, y, sep=':')
+
+# 풀이 3 
+a, b = input().split(':')
+print(int(a), int(b), sep=':')
