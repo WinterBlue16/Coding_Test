44fbd83f8ea14c14ca0bfc466ce7a42d365aaaa1
Winter Lee <leekh090163@gmail.com>
2020-03-13 21:52:00 +0900

    54번 문제

--- 1054.py
+++ 1054.py
@@ -1,2 +1,16 @@
 # 풀이 1
 a, b = map(int, input().split())
+a = int(bool(a))
+b = int(bool(b))
+print(a and b)
+
+# 풀이 2
+a, b = map(int, input().split())
+
+def torf(a, b):
+    if a==1 and b==1:
+        return 1
+    else:
+        return 0
+    
+print(torf(a, b))
