4f4ce631589327df8aae5454681cbb923962d392
Winter Lee <leekh090163@gmail.com>
2020-03-18 23:33:35 +0900

    73번 문제

---
+++ 1073.py
@@ -0,0 +1,17 @@
+# 풀이 1
+num = map(int, input().split())
+num = list(num)
+for i in num:
+    if i != 0:
+        print(i)
+    else:
+        break
+    
+    
+num = map(int, input().split())
+num = list(num)
+for i in num:
+    if i != 0:
+        print(i)
+    else:
+        break
