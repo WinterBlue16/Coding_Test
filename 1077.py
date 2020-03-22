22e5fee5fe32c11af66bc89caa7b34e50fca3ad7
Winter Lee <leekh090163@gmail.com>
2020-03-22 21:32:39 +0900

    77번  문제

---
+++ 1077.py
@@ -0,0 +1,18 @@
+# 풀이 1
+a = int(input())
+li=[]
+for i in range(a+1):
+    li.append(i)
+for i in li:
+    print(i)    
+    
+# 풀이 2
+a = int(input())
+li = list(range(a+1))
+for i in li:
+    print(i)
+    
+# 풀이 3
+a = int(input())
+for i in range(a+1):
+    print(i)
